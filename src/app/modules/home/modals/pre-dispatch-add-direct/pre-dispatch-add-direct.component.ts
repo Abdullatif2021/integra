34de85287be3abcc78fe5db43e397027e6b2de4e
Bahaa Kiddo <Bahaa.aldin.kiddo@gmail.com>
2020-05-17 19:09:17 +0300

    - things

---
+++ src/app/modules/home/modals/pre-dispatch-add-direct/pre-dispatch-add-direct.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import {ModalComponent} from '../modal.component';
+import {ActionsService} from '../../../../service/actions.service';
+import {FiltersService} from '../../../../service/filters.service';
+import {ProductsService} from '../../../../service/products.service';
+import {PreDispatchService} from '../../../../service/pre-dispatch.service';
+
+@Component({
+  selector: 'app-pre-dispatch-add-direct',
+  templateUrl: './pre-dispatch-add-direct.component.html',
+  styleUrls: ['./pre-dispatch-add-direct.component.css']
+})
+export class PreDispatchAddDirectComponent extends ModalComponent implements OnInit {
+    constructor(
+        private actionsService: ActionsService,
+        public productsService: ProductsService,
+        public filtersService: FiltersService,
+        private preDispatchService: PreDispatchService
+    ) {
+        super();
+    }
+
+    name = '' ;
+    error: any = false ;
+    filtersCount = 0 ;
+    selectedCount = 0;
+    confirmed = false ;
+
+    ngOnInit() {
+        this.filtersCount = Object.keys(this.filtersService.filters).length;
+        this.selectedCount = this.productsService.selectedProducts.length ;
+        if (this.filtersService.specials.cities && !this.filtersService.specials.cities.all) { this.filtersCount++;}
+        if (this.filtersService.specials.streets && !this.filtersService.specials.streets.all) { this.filtersCount++;}
+    }
+
+    run(modal) {
+        console.log('sdfsd', this.preDispatchService.getActivePreDispatch());
+        this.actionsService.addToPreDispatch(this.data, this.preDispatchService.getActivePreDispatch()) ;
+        modal.close();
+    }
+}
---
+++ src/app/modules/pages/components/pre-dispatch-log/pre-dispatch-log.component.ts
@@ -0,0 +1,55 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {PreDispatchService} from '../../../../service/pre-dispatch.service';
+import {takeUntil} from 'rxjs/internal/operators';
+import {Subject} from 'rxjs';
+import {ActivatedRoute} from '@angular/router';
+import {IntegraaModalService} from '../../../../service/integraa-modal.service';
+
+@Component({
+  selector: 'app-pre-dispatch-log',
+  templateUrl: './pre-dispatch-log.component.html',
+  styleUrls: ['./pre-dispatch-log.component.css']
+})
+export class PreDispatchLogComponent implements OnInit, OnDestroy {
+
+  constructor(
+      private route: ActivatedRoute,
+      private preDispatchService: PreDispatchService,
+      private integraaModalService: IntegraaModalService,
+  ) {}
+
+  log = [];
+  loading = true ;
+  id = null ;
+  unsubscribe: Subject<void> = new Subject();
+
+  ngOnInit() {
+    this.route.params.subscribe(params => { this.id = params['id']; this.loadLog(); });
+  }
+
+  loadLog() {
+    this.preDispatchService.getLog(this.id).pipe(takeUntil(this.unsubscribe)).subscribe(
+        res => {
+          this.log = res.data;
+          console.log(this.log, res);
+          this.loading = false ;
+        }, error => {
+          this.loading = false;
+        }
+    );
+  }
+
+
+  showLog(id) {
+      this.integraaModalService.open(`/pages/pre-dispatch/${id}/log`, {width: 900, height: 600}, {});
+  }
+
+  expand(item) {
+      item.expanded = !(item.expanded);
+  }
+
+  ngOnDestroy() {
+      this.unsubscribe.next();
+      this.unsubscribe.complete();
+  }
+}
